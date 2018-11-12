b8dc6de04c7c3c513bd061a6626119d161f50666
Jayson Tian <jayson.tian@10275.local>
2018-11-12 17:43:30 -0500

    November 13

---
+++ Skill-listsort.py
@@ -0,0 +1,17 @@
+
+#List Of Strings
+listOfStrings = ['cat','apple','dog','banana']
+
+print (listOfStrings)
+#Sorting alphabetically
+listOfStrings.sort()
+
+print (listOfStrings)
+
+#Sorting by length
+listOfStrings.sort(key=len)
+print (listOfStrings)
+
+##Reverse Alphabetical
+listOfStrings.sort(reverse=True)
+print (listOfStrings)
