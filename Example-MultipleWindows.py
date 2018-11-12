3ce00291faac47b7f789ee3af4745034611d84b3
Jayson Tian <jayson.tian@10275.local>
2018-11-12 18:05:26 -0500

    October 13

---
+++ Example-MultipleWindows.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+LARGE_FONT= ("Verdana", 20)
+
+
+def enter():
+    root2 = tk.Tk()
+
+    btn2 = tk.Button(root, text="Open New Window", root2.destroy())
+
+
+
+root = tk.Tk() #'Tk()' consturcts the window
+root.title("Windows")
+
+labr = tk.Label(root, text="A NEW WINDOW", font=LARGE_FONT)
+labr.pack(pady = 30, padx = 30)
+
+btn = tk.Button(root, text="Open New Window", command=enter)
+btn.pack(pady = 30, padx = 30)
+
+
+
+root.mainloop()
+           
